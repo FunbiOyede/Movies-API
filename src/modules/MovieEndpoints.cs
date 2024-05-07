d85346b6a489317ee3396483bf6e4bea2955b5b3
SamsonOyede <oyedetobi49@gmail.com>
2024-05-07 22:02:01 +0100

    Add Endpoints

--- src/modules/MovieEndpoints.cs
+++ src/modules/MovieEndpoints.cs
@@ -5,7 +5,8 @@ public class MovieEndpoints : CarterModule
 {
     public override void AddRoutes(IEndpointRouteBuilder app)
     {
-        app.MapGet("/shoe", () => "Movies API up and running");
-        app.MapGet("/ball", () => "Ball is up and running");
+        app.MapGet("/movies", () => "Movies API up and running");
+        app.MapGet("/year/{release_year}", (int release_year) => $"Release Year is {release_year}");
+        app.MapGet("/overview/{movie_name}", (string movie_name) => $" Movie Name is  {movie_name}");
     }
 }
